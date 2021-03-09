113f48ddf33920fa918e55568c1fd2210db5463a
nicolas-barragan1018 <nicolas8502@gmail.com>
2021-03-09 17:58:53 -0500

    IndexLayout

---
+++ app/src/main/java/co/edu/unipiloto/beeradviser/MainActivity.java
@@ -0,0 +1,24 @@
+package co.edu.unipiloto.beeradviser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickFindBeer(View view) {
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.color);
+        String beerType = String.valueOf(color.getSelectedItem());
+        brands.setText(beerType);
+    }
+}
